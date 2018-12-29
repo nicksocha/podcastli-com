4d4e02cc8ce6bb13fbf69988be2fb64c35e8057a
nicksocha <38092580+nicksocha@users.noreply.github.com>
2018-12-29 00:10:51 -0600

    Update v1.3
    Second attempt to add list items. Updated classes, IDs, and README.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Podcast < li >
 
-Podcastli.com is a website you can upload your xml file to and get back a list.
+Podcastli.com is a website you can upload an xml file and get back a list.
 
 Check out the live version [here](https://nicksocha.github.io/podcastli-com/).
--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ function onChange(event) {
   const file = event.target.files[0];
   const reader = new FileReader();
   reader.onload = function(event) {
-    // Create a new li element
-    const addListItem = document.createElement("li");
-    // Add some content
-    const addContent = document.createTextNode(event.target.result);
-    // Add the text node to the newly created li
-    addListItem.appendChild(addContent);
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(addListItem, currentDiv);
+    const unorderedList = document.querySelector(".ulist");
+    // Create li element
+    const listItem = document.createElement("li");
+    // Add class
+    listItem.className = "litem";
+    // Create text node and append to li
+    listItem.appendChild(document.createTextNode(event.target.result));
+    // Append li to ul
+    unorderedList.appendChild(listItem);
     console.log(event.target.result);
   };
 
