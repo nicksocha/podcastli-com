f383952c60d99b8611d34eaac2b70aa148c6f84c
nicksocha <38092580+nicksocha@users.noreply.github.com>
2018-12-29 23:05:26 -0600

    Update v1.4
    Added DOMParser. Able to get outline tag [1] text attribute added to li.

--- js/app.js
+++ js/app.js
@@ -2,16 +2,22 @@ function onChange(event) {
   const file = event.target.files[0];
   const reader = new FileReader();
   reader.onload = function(event) {
+    const xmlString = event.target.result;
+    const parser = new DOMParser();
+    const xmlParse = parser.parseFromString(xmlString, "text/xml");
     const unorderedList = document.querySelector(".ulist");
     // Create li element
     const listItem = document.createElement("li");
     // Add class
     listItem.className = "litem";
     // Create text node and append to li
-    listItem.appendChild(document.createTextNode(event.target.result));
+    listItem.appendChild(
+      document.createTextNode(
+        xmlParse.getElementsByTagName("outline")[1].getAttribute("text")
+      )
+    );
     // Append li to ul
     unorderedList.appendChild(listItem);
-    console.log(event.target.result);
   };
 
   reader.readAsText(file);
